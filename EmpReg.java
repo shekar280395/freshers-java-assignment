c737b961043e4fe69f653fa12d6d3c3f9ed06aa6
shekar280395 <shekar280395@users.noreply.github.com>
2016-04-27 23:22:18 -0700

    Update EmpReg.java

--- EmpReg.java
+++ EmpReg.java
@@ -1,4 +1,5 @@
-package s;
+
+	package s;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,8 +14,7 @@ public class EmpReg {
 	System.out.println("a.shekar");
 	System.out.println("b.rakesh");
 	System.out.println("c.ragu");
-	 InputStreamReader isr=new InputStreamReader(System.in);
-     BufferedReader br=new BufferedReader(isr);
+	
      char ch=(char)br.read( );
 		System.out.println("you entered : "+ch);
 		 switch(ch)
@@ -22,40 +22,39 @@ public class EmpReg {
 	        case 'a' :
 	        	System.out.println("employe name is shekar");
 	        	System.out.println("enter the id");
-	        	 InputStreamReader isr1=new InputStreamReader(System.in);
-	             BufferedReader br1=new BufferedReader(isr1);
-	             double id = Double.parseDouble(br1.readLine());
+	        	
+	             double id = Double.parseDouble(br.readLine());
 	        		
 	          if(id==1234){
 	        	  System.out.println("employe log in sucessesfull ");
+	        	  CustomerDe.cart();
 	          }else{
 	        	  System.out.println("login is failed");
-	          }
-	     
-	         
-	           break;
+	               }
+	     break;
 	        case 'b' :
 	        	System.out.println("employe name is rakesh");
 	        	System.out.println("enter empid");
-	        	 InputStreamReader isr2=new InputStreamReader(System.in);
-	             BufferedReader br2=new BufferedReader(isr2);
-	             double id1= Double.parseDouble(br2.readLine());
+	        	 
+	             double id1= Double.parseDouble(br.readLine());
 	        	if(id1==123){
 	        		  System.out.println("employe log in sucessesfull ");
+	        		  CustomerDe.cart();
 		          }else{
 		        	  System.out.println("login is failed");
+		        	  
 		          }
 	        	
 	        	break;
 	        case 'c' :
 	           System.out.println("emp name is ragu");
 	           System.out.println("enter emp id");
-	         InputStreamReader isr3=new InputStreamReader(System.in);
-	             BufferedReader br3=new BufferedReader(isr3);
-	             double id3 = Double.parseDouble(br3.readLine());
+	        
+	             double id3 = Double.parseDouble(br.readLine());
 	           if(id3==345){
 	        	   
 		        		  System.out.println("employe log in sucessesfull ");
+		        		  CustomerDe.cart();
 			          }else{
 			        	  System.out.println("login is failed");
 			          }
@@ -70,6 +69,14 @@ public class EmpReg {
 	
 	}
 		
+
+		
+	
+	
+
+
+
+	
 		
 		
 	
