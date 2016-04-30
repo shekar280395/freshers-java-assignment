5ca61dd833c491c9e3ef69f6157355983a4965d8
shekar280395 <shekar280395@users.noreply.github.com>
2016-04-29 23:41:09 -0700

    Update Use2.java

--- Use2.java
+++ Use2.java
@@ -1,3 +1,4 @@
+
 package s;
 
 import java.io.BufferedReader;
@@ -6,9 +7,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Use2 {
+public class Use2 {/*if new user reg process*/
 
-	public static void addDet() throws NumberFormatException, IOException {
+	public static void addDet() throws NumberFormatException, IOException   {
 		
 			
 			
@@ -23,7 +24,7 @@ public class Use2 {
 
 						System.out.println("new reg process");
 						System.out.println("1.ADD user ");
-						System.out.println("2.LIST PRODUCT ");
+						System.out.println("2.LIST users ");
 						System.out.println("3.Exit");
 
 						System.out.println("Enter Option from 1 - 3: ");
@@ -31,7 +32,7 @@ public class Use2 {
 						int ch = sc.nextInt();
 						switch (ch) {
 						case 1:
-							System.out.println("Enter user new Id, Name and price: ");
+							System.out.println("Enter user new Id, Name and phone number: ");
 
 							int nCusId = Integer.parseInt(br.readLine());
 							String nName = br.readLine();
@@ -40,15 +41,18 @@ public class Use2 {
 							boolean status = ops.addUser(prod,cusad,nPhno);
 							if (status != true) {
 								System.out.println("user Already Exists");
+								Use.cart1();
 							} else {
 								System.out.println("user added successfully");
-								Use.cartA();
+								Use.cart2();
+								LogOutCus.logOut();
 							}
-
+							System.exit(0);
 							break;
 						case 2:
 							ops.list(cusad);
-							/*b.totalPric();*/
+						/*	b.totalPric();*/
+							
 						case 3:
 							System.exit(0);
 							break;
